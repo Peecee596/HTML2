aada057437e5495b8912b6e48f24f7ab4553ddff
poonam <poonam.chauhan229@gmail.com>
2021-09-22 16:08:51 +0530

    lec 13

---
+++ ReactJS/6createcomponents/my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AppHeader from './Components/AppHeader';
+import AppBody from './Components/AppBody';
+import AppFooter from './Components/AppFooter';
+
+function App() {
+  return (
+    <div className="App">
+     <AppHeader/> 
+     <AppBody/>
+     <AppFooter/>
+    </div>
+  );
+}
+
+export default App;
