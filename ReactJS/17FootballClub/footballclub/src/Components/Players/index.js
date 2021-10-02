9a6160ee1fd44c85de1e15d5c6cf80f5acdf3d8c
poonam <poonam.chauhan229@gmail.com>
2021-10-02 12:18:35 +0530

    jsproject1

--- ReactJS/17FootballClub/footballclub/src/Components/Players/index.js
+++ ReactJS/17FootballClub/footballclub/src/Components/Players/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
-export default function Players(){
+import './index.css'
+import{ Link } from 'react-router-dom'
+export default function Players(props){
+    const renderPlayers=()=>{
+        return props.players.map((player)=>(
+        <li><Link to ={"/players" +"/" +player.id +"/" +player.name +"/" +player.image}>{player.name}</Link></li>
+        
+        ))
+    }
     return(
-        <h1>Our Top Players</h1>
+       <div>
+            <h1>Our Top Players</h1>
+            <ul className='playerslist'>
+                {renderPlayers()}
+            </ul>
+       </div>
     );
 }
--- ReactJS/17FootballClub/footballclub/src/Router.js
+++ ReactJS/17FootballClub/footballclub/src/Router.js
@@ -1,22 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import {Switch,Route} from 'react-router-dom'
 import './App';
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Players from "./Components/Players";
 import Navigation from "./Components/Navigation";
+// import Ronaldo from './assets/images/Ronaldo.jpg'
+// import messi from './assets/images/messi.jpg'
+// import Pogba from './assets/images/Pogba.jpg'
+// import neymar from './assets/images/neymar.jpg'
+// import lampard from './assets/images/lampard.jpg'
+import PlayerContainer from "./Container/PlayerContainer";
+export default class Router extends Component{
 
-export default function Router(){
-    return(
-        <div>
-            <Navigation/>
-            <Switch>
-               
-                <Route exact path='/about' component ={About}/>
-                <Route exact path='/contact' component ={Contact}/>
-                <Route exact path='/players' component ={Players}/>
-            </Switch>
+    state={
+        players:[
+            {
+                'id':1,
+                'name':'Ronaldo',
+                'image':'Ronaldo.jpg'
+            },
+            {
+                'id':2,
+                'name':'Messi',
+                'image':'messi.jpg'
+            },
+            
+            {
+                'id':3,
+                'name':'Pogba',
+                'image':'Pogba.jpg'
+            },
 
-        </div>
-    )
+            {
+                'id':4,
+                'name':'Frank Lampard',
+                'image':'lampard.jpg'
+            },
+            
+            {
+                'id':5,
+                'name':'Neymar',
+                'image':'neymar.jpg'
+            }
+        ]
+    }
+
+    render(){
+                return(
+                    <div>
+                        <Navigation/>
+                        <Switch>
+                        
+                            <Route exact path='/about' component ={About}/>
+                            <Route exact path='/contact' component ={Contact}/>
+                            <Route exact path='/players'                            
+                            render={(props)=><Players {...props}players={this.state.players}/>}/>
+                            <Route exact path="/players/:id/:name/:image" render={PlayerContainer}/>
+                        </Switch>
+
+                    </div>
+                )
+            }
 }
