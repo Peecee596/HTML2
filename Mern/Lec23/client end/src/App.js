033ff221dd614a629851afc9b000c82acc73e693
Poonam <poonam.chauhan229@gmail.com>
2022-02-15 14:23:08 +0530

    lec23

---
+++ Mern/Lec23/client end/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi Tailwind</h1>
+      <button class='spinner'>Loading</button>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
